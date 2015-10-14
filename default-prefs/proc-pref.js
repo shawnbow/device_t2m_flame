d8ad593d6848985385ffe45146d7490c16558ba9
Danny Liang <danny.liang@acadine.com>
2015-10-14 11:56:16 +0800

    CORE-910: Part 1 - Add preference file support on Flame. r=seinlin.

---
+++ default-prefs/proc-pref.js
@@ -0,0 +1,6 @@
+/* Copyright (C) 2015 Acadine Technologies. All rights reserved. */
+
+#ifdef MOZ_WIDGET_GONK
+// This preference indicates the name of GPU device.
+pref("device.gpu.nodename", "fdc00000.qcom,kgsl-3d0");
+#endif
